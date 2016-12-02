618ff9ac1190e2774ccb67b902e9d2746ded499b
Rahul <ramra20@ca.com>
2016-12-02 09:51:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+var stories = [{
+  "header": "Ad Blockers: Setback or Spark for Innovation?",
+  "image": "story.png",
+  "content": "Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!"
+}, {
+  "header": "The real reasons why making money with online media is hard",
+  "image": "story.png",
+  "content": "Hi All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!Hello All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!"
+}, {
+  "header": "How Motherly Uses Personalization to Create Engagement",
+  "image": "story.png",
+  "content": "Hey All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!"
+}, {
+  "header": "Fixing email overload and now chat overload, for content teams",
+  "image": "story.png",
+  "content": "Yo All,\nFirstly I want to thank you Rhonda and her team for the amazing book, The Secret, that they have shared with the world. I am also grateful for my friend who introduced me to The Secret in 2013 when I was in such dire need of it. When I read a few pages my life had changed. I used The Secret to get me The Secret book itself and I got it exactly the way I had imagined. Thank you, thank you, thank you for this amazing gift.\nRecently I and my family were planning a vacation but it did not materialize. Then one fine day I sat down and wrote with heartfelt gratitude about how I would like my travel plans to work out before June 30th. And, to my surprise, I am just back from my dream vacation, exactly the way I had imagined it and it worked out before June 30!! I am truly mesmerized!"
+}];
+
+var closeModal = function() {
+  console.log("hi");
+  var storyContent = document.getElementById("story-content");
+  document.getElementById("story").removeChild(storyContent);
+  document.getElementById("story-cover").removeAttribute("class");
+};
+
+
+var expandStory = function(id) {
+  document.getElementById('story-heading').scrollTop = 0;
+  var header = document.getElementById("story-heading");
+  header.innerText = stories[id].header;
+  document.getElementById("story").appendChild(header);
+  var paragraphs = stories[id].content.split('\n');
+  var storyContent = document.createElement("div");
+  storyContent.id = "story-content";
+  paragraphs.forEach(function(paragraph, index) {
+    var para = document.createElement("p");
+    para.innerText = paragraph;
+    storyContent.appendChild(para);
+  });
+  document.getElementById("story").appendChild(storyContent);
+  document.getElementById("story-cover").setAttribute("class",
+    "stroy display-modal");
+
+  document.onkeydown = function(evt) {
+    if (evt.keyCode === 27) {
+      closeModal();
+    }
+  };
+};
+window.onload = function() {
+  var storiesList = document.getElementById('stories-list');
+  if (stories[0]) {
+    createArticle(stories[0], 0);
+  }
+  stories.slice(1, stories.length).forEach(function(value, index) {
+    createArticle(value, index + 1);
+  });
+
+  function createArticle(stioryDetails, id) {
+    var story = document.createElement("article");
+    var header = document.createElement("h3");
+    header.innerText = stioryDetails.header;
+    var img = document.createElement("img");
+    img.src = stioryDetails.image;
+    img.alt = stioryDetails.header;
+    story.id = id;
+    story.appendChild(header);
+    story.appendChild(img);
+    storiesList.appendChild(story);
+    img.onload = function() {
+      document.getElementsByTagName("body")[0].style.display = "block";
+    };
+    story.addEventListener("click", function() {
+      expandStory(this.id);
+    });
+  }
+  var storyCover = document.getElementById("story-cover");
+
+  window.onclick = function(eve) {
+    console.log(eve);
+  };
+  storyCover.onclick = function(event) {
+    console.log(event);
+    if (event.target === document.getElementById("close-story") || event.target ===
+      document.getElementById("story-cover")) {
+      closeModal();
+    }
+  };
+};
